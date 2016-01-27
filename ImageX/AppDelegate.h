6099379f05a567b211a2bcc0876b96e8b01f10d3
Khang Hoang <hoangtrieukhang@gmail.com>
2016-01-27 21:17:38 +0700

    Init project

---
+++ ImageX/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageX
+//
+//  Created by ZALORA on 1/27/16.
+//  Copyright © 2016 KH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
