7fb3837ec54a5a4e68a4034290078b05be74d80f
Khang Hoang <hoangtrieukhang@gmail.com>
2016-01-30 20:44:55 +0700

    handle format datetime

--- ImageX/ViewControllers/Details/KHDetailsViewController.m
+++ ImageX/ViewControllers/Details/KHDetailsViewController.m
@@ -14,6 +14,7 @@
 @property (weak, nonatomic) IBOutlet UIImageView *mainImage;
 @property (weak, nonatomic) IBOutlet UIImageView *userAvatar;
 @property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *datetimeLabel;
 
 @property (weak, nonatomic) IBOutlet UIView *userView;
 @property (weak, nonatomic) IBOutlet UITextView *textViewDescription;
@@ -28,21 +29,34 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
+    // add guesture to userview
     UITapGestureRecognizer *tapOnUserView = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onUserView)];
     [self.userView addGestureRecognizer:tapOnUserView];
     
+    // set main image
     [self.mainImage sd_setImageWithURL:[NSURL URLWithString:[self.image[@"image_url"] firstObject]]];
     [self.userAvatar sd_setImageWithURL:self.image[@"user"][@"avatars"][@"default"][@"https"]];
     self.usernameLabel.text = self.image[@"user"][@"fullname"];
     
+    // round the avatar
     self.userAvatar.layer.cornerRadius = 20;
     self.userAvatar.layer.masksToBounds = YES;
     
+    // format the createTime
+    NSDate *createdAt = [[KHDateFormatterManager sharedInstance].formatter dateFromString:self.image[@"created_at"]];
+    self.datetimeLabel.text = [self displayCreatedTime:createdAt];
     
-//    self.textViewDescription.text = self.image[@"description"];
     self.textViewDescription.text = @"dummy description";
+    
+    // adjust the height of the desciption
     CGSize sizeThatFitsTextView = [self.textViewDescription sizeThatFits:CGSizeMake(self.textViewDescription.frame.size.width, MAXFLOAT)];
     self.descriptionHeightConstrant.constant = sizeThatFitsTextView.height;
+}
+
+- (NSString *)displayCreatedTime:(NSDate *)date {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"EEE, dd MMM yyyy";
+    return [formatter stringFromDate:date];
 }
 
 - (void)onUserView {
